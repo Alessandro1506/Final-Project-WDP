d96a5e6faa22e4b38cdeadd02e4a5c0ce403253b
Halvin Silva Mayes <Hsilvamayes8393@conestogac.on.ca>
2025-11-26 22:37:20 -0500

    Initial commit solution with two projects

---
+++ MessagingClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MessagingClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
